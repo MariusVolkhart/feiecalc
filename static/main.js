ede755cb859bbed74ab7a839046878b53aef312e
Shane Reustle <me@shanereustle.com>
2016-12-16 02:11:31 +0900

    New trip form ; Load trips from local storage

--- static/main.js
+++ static/main.js
@@ -1,35 +1,88 @@
+// Models
+var TripModel = Backbone.Model.extend();
+// Collections
 
-var init = function(){
-    // Initialize the app
+var TripsCollection = Backbone.Collection.extend({
     
-    var currentYear = new Date().getFullYear();
+    model: TripModel,
+    localStorage: new Backbone.LocalStorage('trips')
+    
+});
 
-    // Init Calendar
-    $('#calendar').calendar({ 
-        style: 'background',
-        alwaysHalfDay: true,
-        enableRangeSelection: true,
-        mouseOnDay: mouseOnDay,
-        mouseOutDay: function(e) {
-            if(e.events.length > 0) {
-                $(e.element).popover('hide');
-            }
-        },
-        dataSource: [
-            {
-                country: 'Malaysia',
-                startDate: new Date(currentYear, 1, 4),
-                endDate: new Date(currentYear, 3, 5)
-            },
-            {
-                country: 'Japan',
-                startDate: new Date(currentYear, 3, 5),
-                endDate: new Date(currentYear, 3, 15)
-            }
-        ]
-    });
+// Views
+var TripFormView = Backbone.View.extend({
+    template: _.template($('#template-trip-form').html()),
     
-}
+    events: {
+        'click button[data-action=save]': function(e){
+            this.saveTrip();
+        }
+    },
+    
+    initialize: function(options){
+        this.render();
+    },
+    
+    render: function(){
+        this.$el.find('.modal-body').html(this.template({
+            countries: COUNTRIES
+        }));
+    },
+    
+    showNew: function(startDate, endDate){
+        this.$el.find('[data-field=trip-start-date]').val(startDate.format('YYYY-MM-DD'));
+        this.$el.find('[data-field=trip-end-date]').val(endDate.format('YYYY-MM-DD'));
+        $('#trip-modal').modal('show');
+    },
+    
+    showExisting: function(){
+    },
+    
+    validate: function(values){
+        // Validate the trip details
+        
+        if(!values.country){
+            alert('Please select a country');
+            return false;
+        }
+        
+        if(!values.startDate._isValid || values.startDate._i.length != 10){
+            alert('Please enter a valid start date (format: YYYY-MM-DD)');
+            return false;
+        }
+        
+        if(!values.endDate._isValid || values.endDate._i.length != 10){
+            alert('Please enter a valid end date (format: YYYY-MM-DD)');
+            return false;
+        }
+        
+        return true;
+    },
+    
+    saveTrip: function(){
+        // Save the trip
+
+        var values = {
+            country: this.$el.find('[data-field=trip-country]').val(),
+            startDate: moment(this.$el.find('[data-field=trip-start-date]').val()),
+            endDate: moment(this.$el.find('[data-field=trip-end-date]').val())
+        };
+        
+        // Validate it first
+        if(!this.validate(values)){
+            return;
+        }
+        
+        // Save the trip
+        var trip = new TripModel(values);
+        tripsColl.add(trip);
+        trip.save();
+        
+        // Close the modal
+        this.$el.modal('hide');
+        this.$el.find('[data-field=trip-country]').val('');
+    }
+});
 
 // Calendar Helpers
 var mouseOnDay = function(e){
@@ -40,17 +93,17 @@ var mouseOnDay = function(e){
             var startDate =  moment(e.events[i].startDate);
             var endDate =  moment(e.events[i].endDate);
             var tripDuration = moment.duration(startDate.diff(endDate));
-            tripDuration = (tripDuration.asDays() * -1);
+            tripDuration = (tripDuration.asDays() * -1)-1;
             
             content += '\
                 <div class="event-tooltip-content">\
-                    <div class="event-name" style="color:'
-                        + e.events[i].color + 
-                        '">'
-                        + e.events[i].country +
+                    <div class="event-name" style="color:' +
+                        e.events[i].color + 
+                        '">' +
+                        e.events[i].country +
                     '</div>' +
-                    '<div class="event-location">'
-                        + tripDuration + ' days'
+                    '<div class="event-location">' +
+                        tripDuration + ' full days'
                     '</div>' +
                 '</div>';
         }
@@ -65,6 +118,50 @@ var mouseOnDay = function(e){
         $(e.element).popover('show');
     }
 }
-       
-init();
 
+
+// Init    
+
+// Load the trips collection
+var tripsColl = new TripsCollection();
+
+// Draw trip form
+var tripForm = new TripFormView({
+    el: $('#trip-modal'),
+    collection: tripsColl
+});
+
+// Init Calendar
+var currentYear = new Date().getFullYear();
+$('#calendar').calendar({ 
+    style: 'background',
+    alwaysHalfDay: true,
+    enableRangeSelection: true,
+    mouseOnDay: mouseOnDay,
+    mouseOutDay: function(e){
+        if(e.events.length > 0){
+            $(e.element).popover('hide');
+        }
+    },
+    selectRange: function(e){
+        tripForm.showNew(moment(e.startDate), moment(e.endDate));
+    },
+    dataSource: []
+});
+
+// Update the calendar every time the trips collection changes
+tripsColl.on('change reset', function(){
+    var tripsList = [];
+    this.toJSON().forEach(function(trip){
+        tripsList.push({
+            country: _.find(COUNTRIES, {code: trip.country.toUpperCase()}).name,
+            startDate: moment(trip.startDate)._d,
+            endDate: moment(trip.endDate)._d
+        });
+    });
+    
+    $('#calendar').data('calendar').setDataSource(tripsList);
+});
+
+// Load trips
+tripsColl.fetch({ reset: true });
